4518567155d8773ad75df05f0568a32e60384cf3
okrueg <opkruege@asu.edu>
2025-10-17 21:40:06 -0700

    Button paddings
    - added 20 length button paddings

--- main.py
+++ main.py
@@ -10,4 +10,7 @@ click_button = tk.Button(
     height=2
 )
 
+# Place the button at the bottom of the window
+# pady adds a little vertical space for better looks
+click_button.pack(side=tk.BOTTOM, pady=20)
 window.mainloop()
